d791237f742459625bac1bb4bac360ba77b76ce7
Sycrusian <sycrus@protonmail.com>
2024-03-28 15:21:36 -0300

    S1-08 | 💪 Atividade: Exercitando Loops e Condicionais

--- main.py
+++ main.py
@@ -14,9 +14,49 @@ def bhaskara(a, b, c):
     return (round(x1, 2), round(x2, 2))
 
 
-print(bhaskara(7, 3, 4))
-# Delta Negativo
-print(bhaskara(1, 5, 2))
-# x1=-0.44, x2=-4.56
-print(bhaskara(3, 10, 2))
-# x1=-0.21, x2=-3.12'
+# if __name__ == "__main__":
+#     print(bhaskara(7, 3, 4))
+#     # Delta Negativo
+#     print(bhaskara(1, 5, 2))
+#     # x1=-0.44, x2=-4.56
+#     print(bhaskara(3, 10, 2))
+#     # x1=-0.21, x2=-3.12'
+
+def corresponding_parenthesis(text):
+    result = ""
+    for i in range(len(text)):
+        if i < len(text) - 1:
+            if text[i] == "(":
+                if text[i+1] == ")":
+                    continue
+        if i > 0:
+            if text[i] == ")":
+                if text[i-1] == "(":
+                    continue
+        result += text[i]
+    return result
+
+def remove_more_than_two_repetitions(text):
+    result = ""
+    for i in range(len(text)):
+        if i > 1:
+            if text[i] == text[i-1]:
+                if text[i] == text[i-2]:
+                    continue
+        result += text[i]
+    return result
+
+if __name__ == "__main__":
+    # Exemplo 1
+    result = corresponding_parenthesis("()()")
+    print(result)   # ''
+    # Exemplo 2
+    result = corresponding_parenthesis("()))")
+    print(result)   # '))'
+    # Exemplo 3
+    result = corresponding_parenthesis("()()())()()(()((")
+    print(result)   # ')((('
+    
+    text = "Ollloco meuuuu, taaa peegaando fogoo biiiiichooo"
+    text = remove_more_than_two_repetitions(text)
+    print(text)     # 'Olloco meuu, taa peegaando fogoo biichoo'
